9f50554c885fcf93c35647e50a5001e3f78bc565
Bruce Ediger <bediger8@gmail.com>
2021-11-08 19:42:50 -0700

    other base64 package encoders

--- encode64.go
+++ encode64.go
@@ -8,5 +8,9 @@ import (
 
 func main() {
 	encodedString := base64.StdEncoding.EncodeToString([]byte(os.Args[1]))
-	fmt.Println(encodedString)
+	fmt.Printf("StdEncoding:     %s\n", encodedString)
+	encodedString = base64.RawStdEncoding.EncodeToString([]byte(os.Args[1]))
+	fmt.Printf("Raw StdEncoding: %s\n", encodedString)
+	encodedString = base64.RawURLEncoding.EncodeToString([]byte(os.Args[1]))
+	fmt.Printf("URL StdEncoding: %s\n", encodedString)
 }
